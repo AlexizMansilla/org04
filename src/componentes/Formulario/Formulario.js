17d054990972e71ebca67f19b92a394abd6dcf49
AlexizMansilla <aggroman777@gmail.com>
2025-01-11 03:00:46 -0300

    creacion de evento

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -4,14 +4,22 @@ import ListaOpciones from "../ListaOpciones"
 import Boton from "../Boton"
 
 const Formulario = () => {
+
+    const manejarEnvio = (e) => {
+        e.preventDefault()
+        console.log("Manejo el envio", e)
+    }
+
     return <section className="formulario">
-        <form>
+        <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador.</h2>
             <CampoTexto titulo="Nombre"placeholder="Ingresar nombre"/>
             <CampoTexto titulo="Puesto" placeholder="Ingresar puesto"/>
             <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto"/>
             <ListaOpciones/>
-            <Boton texto ="Crear"/>
+            <Boton>
+                Crear
+            </Boton>
         </form>    
     </section>
 }
