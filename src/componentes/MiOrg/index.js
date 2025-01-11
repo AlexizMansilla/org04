d1b5e54bc5bfa408083b739ed59e82265e42ad71
AlexizMansilla <aggroman777@gmail.com>
2025-01-11 16:45:15 -0300

    configuraciones de boton

--- src/componentes/MiOrg/index.js
+++ src/componentes/MiOrg/index.js
@@ -1,8 +1,18 @@
+import { useState } from 'react';
 import './MiOrg.css'
 const MiOrg = () => {
+    //Estado - hooks
+    //useState
+    //useState()
+    console.log(useState())
+    const manejarClick = () => {
+        console.log("Mostrar/ocultar elemento");
+    }
+
+
     return <section className="orgSection">
         <h3 className="title"> Mi organización</h3>
-        <img src="/img/add.png" alt="add"/>
+        <img src="/img/add.png" alt="add" onClick={manejarClick} />
     </section>
 }
 export default MiOrg
