9da1a64d9906f823ef5d476559578838f54d873e
AlexizMansilla <aggroman777@gmail.com>
2025-01-11 02:05:26 -0300

    se crea boton

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,5 +1,7 @@
 import "./Formulario.css"
-import CampoTexto from "../CampoTexto";
+import CampoTexto from "../CampoTexto"
+import ListaOpciones from "../ListaOpciones"
+import Boton from "../Boton"
 
 const Formulario = () => {
     return <section className="formulario">
@@ -8,7 +10,8 @@ const Formulario = () => {
             <CampoTexto titulo="Nombre"placeholder="Ingresar nombre"/>
             <CampoTexto titulo="Puesto" placeholder="Ingresar puesto"/>
             <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto"/>
-            <CampoTexto titulo="Equipo" placeholder="Selecionar equipo"/>
+            <ListaOpciones/>
+            <Boton texto ="Crear"/>
         </form>    
     </section>
 }
