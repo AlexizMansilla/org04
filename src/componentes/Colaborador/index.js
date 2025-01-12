2f4a76bdb0377961aa4ad54b43a3657d104fd463
AlexizMansilla <aggroman777@gmail.com>
2025-01-12 19:22:35 -0300

    se modifica formularios y se crea codigo de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,15 @@
+import "./Colaborador.css"
+
+const Colaborador = (props) => {
+    return  <div className="colaborador">
+        <div className="encabezado">
+            <img src="https://github.com/AlexizMansilla.png" alt="Alexiz Mansilla" />
+        </div>   
+        <div className="info">
+            <h4>Alexiz Mansilla</h4>
+            <h5>Desarrollador Frontend</h5>
+    </div>
+</div>
+}    
+
+export default Colaborador;
--- src/componentes/ListaOpciones/index.js
+++ src/componentes/ListaOpciones/index.js
@@ -4,15 +4,7 @@ const ListaOpciones = (props) => {
     //Metodo map −> arreglo.map(equipo,index) => {
     //  return <option></option>
     //})
-    const equipos = [
-        'Programación',
-        'Front End',
-        'Data Science',
-        'Devops',
-        'UX y Diseño',
-        'Móvil',
-        'Innovación y Gestión'
-    ]
+    
 
     const manejarCambio = (e) => {
         console.log("Cambio", e.target.value)
@@ -23,7 +15,7 @@ const ListaOpciones = (props) => {
         <label>Equipos</label>
         <select value={props.valor} onChange={manejarCambio}>
             <option value="" disabled defaultValue="" hidden>Seleccionar equipo</option>
-           {equipos.map((equipo,index) => <option key={index}>{equipo}</option>)}
+           {props.equipos.map((equipo,index) => <option key={index}>{equipo}</option>)}
         </select>
     </div>
 }
